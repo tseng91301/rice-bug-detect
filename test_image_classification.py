3f3df820ab1f23e87e22e10428ec63efabed2e6f
Ching-Kai Tseng <tseng91301@gmail.com>
2026-09-02 15:35:53 +0800

    refactor: remove legacy utility functions and unused configuration files

--- test_image_classification.py
+++
@@ -1,4 +0,0 @@
-from cnn.classifier import Classifier
-classifier = Classifier("saved_models/17classes/best_model.pth", "dataset")
-result = classifier.predict("dataset_extend/外米綴蛾-幼蟲/2026-04-13_153101_91c73669-90ec-4d6b-9ea9-90e716235ec0.jpg")
-print(result)
